356e781331c8b726fa47b1f9e3f3b2f7e48597ee
dohaicuong <beatyshot@gmail.com>
2020-05-31 10:47:08 +0000

    Deploying to gh-pages from  @ 4d3c3b5a12c4ea8cf60e1fbe86fbf03c50eff219 🚀

---
+++ precache-manifest.6f59c5b8b0e866e910b3e695434e1004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c596aeafdddb23cbcf63be7ee6fe296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e01ed5e259785801d8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c310fd35fbc74cee7a51",
+    "url": "/static/js/2.77eec517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77eec517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e01ed5e259785801d8",
+    "url": "/static/js/main.3bb8b556.chunk.js"
+  },
+  {
+    "revision": "f6bd60e2bfe86e8c9bc2",
+    "url": "/static/js/runtime-main.3fac27fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
